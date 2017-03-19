72e82f1171b9d488ec84d5177c5485d9f827786b
Chirag Chaplot <chiragchaplot@gmail.com>
2017-03-19 11:11:15 +0300

    Created application with navigation bar
    Starting the app with navigation bar and 2 buttons added

---
+++ Navigation Bar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Navigation Bar
+//
+//  Created by Chirag Chaplot on 3/19/17.
+//  Copyright © 2017 ChazIT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
